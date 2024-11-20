46ab1af7b5e6bed254c2a5899efa6d085c538268
AndoreazNguyen <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-11-20 21:12:41 +0700

    [DAY3] Logging

--- nobsv2/src/main/java/com/example/nobsv2/exception/ProductNotFoundException.java
+++ nobsv2/src/main/java/com/example/nobsv2/exception/ProductNotFoundException.java
@@ -1,12 +1,17 @@
 package com.example.nobsv2.exception;
 
+import com.example.nobsv2.product.services.GetProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 //This is the first way of throwing error if you want to go with default Java Exception handler
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException{
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
     public ProductNotFoundException() {
         super(ErrorMessages.PRODUCT_NOT_FOUND.getMessage());
+        logger.error("Exception " + getClass() + " thrown");
     }
 }
--- nobsv2/src/main/java/com/example/nobsv2/product/services/GetProductService.java
+++ nobsv2/src/main/java/com/example/nobsv2/product/services/GetProductService.java
@@ -5,6 +5,8 @@ import com.example.nobsv2.exception.ProductNotFoundException;
 import com.example.nobsv2.product.ProductReposistory;
 import com.example.nobsv2.product.model.Product;
 import com.example.nobsv2.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,16 @@ public class GetProductService implements Query<Integer, ProductDTO> {
 
     private final ProductReposistory productReposistory;
 
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
+
+
     public GetProductService(ProductReposistory productReposistory) {
         this.productReposistory = productReposistory;
     }
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+        logger.info("Executing " + getClass() + " input: " + input);
         Optional< Product> productOptional = this.productReposistory.findById(input);
         if (productOptional.isPresent()) {
             return ResponseEntity.ok(new ProductDTO(productOptional.get()));
